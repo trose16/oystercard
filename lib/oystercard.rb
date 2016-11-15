bc7f08eb21ec241525c3acc1cc8700a213394d4a
Teako Warfield-Graham <teakowg@gmail.com>
2016-11-15 12:05:52 +0000

    Passed first test

---
+++ lib/oystercard.rb
@@ -0,0 +1,9 @@
+class Oystercard
+
+  attr_reader :money
+
+  def initialize(money=0)
+    @money = money
+  end
+
+end
---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,11 @@
+
+require 'oystercard'
+
+describe Oystercard do
+
+  it 'initial money value is 0' do
+    expect(subject.money).to eq 0
+  end
+
+
+end
